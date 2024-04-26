a781afa8d4ce1ca6bdc4338a1c2b8853ad88a790
kimziyeon <aa40254037@gmail.com>
2024-04-26 19:31:13 +0900

    20240426

--- src/component/Detail.jsx
+++ src/component/Detail.jsx
@@ -12,36 +12,40 @@ function Detail(props) {
     let { id } = useParams()
     console.log(detail.casts);
 
-    if(!detail.title) return;
+    if (!detail.title) return;
 
     return (
-        <div className='detailBack' style={{background:`url(${back + detail.backdrop_path}) 0 0 /cover no-repeat`}} >
-            <div className='detailVeiw'>
-                <p><img src={thumb300 + detail.poster_path}/></p>
-                <div>
-                    <h2>{detail.title}</h2>
-                    
-                    {
-                        detail.genres.map((obj)=>
-                            <span>{obj.name}</span>
-                        )
-                    }
-                    
-                    <h4>{detail.overview}</h4>
-                    <h3>Casts</h3>
+        <div className='detailBack' style={{ background: `url(${back + detail.backdrop_path}) 0 0 /cover no-repeat` }} >
+            <div className='backdrop'>
 
-                    <div className='castView'>
+                <div className='detailVeiw'>
+                    <p><img src={thumb300 + detail.poster_path} /></p>
+                    <div>
+                        <h2>{detail.title}</h2>
+
                         {
-                            detail.casts.cast.map((obj)=>
-                                <figure>
-                                    <img src={thumb200 + obj.profile_path}/>
-                                    <figcaption>{obj.name}</figcaption>
-                                </figure>
+                            detail.genres.map((obj) =>
+                                <span>{obj.name}</span>
                             )
                         }
+
+                        <h4>{detail.overview}</h4>
+                        <h3>Casts</h3>
+
+                        <div className='castView'>
+                            {
+                                detail.casts.cast.map((obj) =>
+                                    <figure>
+                                        <img src={thumb200 + obj.profile_path} />
+                                        <figcaption>{obj.name}</figcaption>
+                                    </figure>
+                                )
+                            }
+                        </div>
+
                     </div>
-                    
                 </div>
+
             </div>
         </div>
     );
